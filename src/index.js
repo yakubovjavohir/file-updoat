c399ad7197e611d6249922cc9e819ea83961e9f9
javohir9999 <javohiryoqubov182@gmail.com>
2024-12-14 13:16:14 +0500

    restart

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ const {Repository} = require("./lib/repository")
 const filePath = path.resolve("database", "file.json")
 const fileRepo = new Repository(filePath)
 
+const img_folder = path.join(__dirname, "../files/img-folder")
+const video_folder = path.join(__dirname, "../files/video-folder")
+const pdf_folder = path.join(__dirname, "../files/pdf-folder")
+
+
 
 const app = express()
 
@@ -20,14 +25,21 @@ app.use(file_upload())
 
 
 
-app.post("/img-updoat", async (req, res) => {
+app.post("/img-upload", async (req, res) => {
     const file = req.files.img;
 
     if (file.mimetype !== "image/png") {
         return res.status(400).json(new ResData(400, "bu IMG emas!"));
     }
 
-    const targetFolder = path.join(__dirname, '../files/img-folder');
+    try {
+        await fs.access(img_folder);
+    } catch (err) {
+        await fs.mkdir(img_folder, { recursive: true });
+    }
+
+
+    const targetFolder = path.join(__dirname, `../files/img-folder`);
     const targetPath = path.join(targetFolder, path.basename(file.name));
 
     try {
@@ -46,14 +58,20 @@ app.post("/img-updoat", async (req, res) => {
 });
 
 
-app.post("/video-updoat", async (req, res)=>{
+app.post("/video-upload", async (req, res)=>{
     const file = req.files.video;
 
     if (file.mimetype !== "video/mp4") {
         return res.status(400).json(new ResData(400, "bu VIDEO emas!"));
     }
 
-    const targetFolder = path.join(__dirname, '../files/video-folder');
+    try {
+        await fs.access(video_folder);
+    } catch (err) {
+        await fs.mkdir(video_folder, { recursive: true });
+    }
+
+    const targetFolder = path.join(__dirname, `../files/video-folder`);
     const targetPath = path.join(targetFolder, path.basename(file.name));
 
     try {
@@ -71,14 +89,20 @@ app.post("/video-updoat", async (req, res)=>{
     }
 })
 
-app.post("/pdf-updoat", async (req, res)=>{
+app.post("/pdf-upload", async (req, res)=>{
     const file = req.files.pdf;
 
     if (file.mimetype !== "application/pdf") {
         return res.status(400).json(new ResData(400, "bu PDF emas!"));
     }
 
-    const targetFolder = path.join(__dirname, '../files/pdf-folder');
+    try {
+        await fs.access(pdf_folder);
+    } catch (err) {
+        await fs.mkdir(pdf_folder, { recursive: true });
+    }
+
+    const targetFolder = path.join(__dirname, `../files/pdf-folder`);
     const targetPath = path.join(targetFolder, path.basename(file.name));
 
     try {
